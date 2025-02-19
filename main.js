be8463f0288b8ed7dfdd3b415f44bff689b0bb93
Danyil-Sikolenko <banielsikolenko@gmail.com>
2025-02-19 12:02:13 +0100

    Basic data types

---
+++ main.js
@@ -0,0 +1,19 @@
+let age = 20;
+console.log(typeof(age));
+
+let name = 'Danyil';
+console.log(typeof(name));
+
+let married = false;
+console.log(typeof(married));
+
+let undefined1 
+console.log(typeof(undefined1));
+
+let Object1 ={
+    x : 10,
+    y : 20,
+    z :30
+};
+
+console.log(typeof(Object1));
