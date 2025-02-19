30f836b90ebaccbf4da71ac4a42a146984a5a509
Danyil-Sikolenko <banielsikolenko@gmail.com>
2025-02-19 14:51:39 +0100

    userProfile

---
+++ main.js
@@ -0,0 +1,11 @@
+let userName = prompt('Write your name :');
+console.log(userName);
+
+let userAge = prompt('How old are you ?');
+console.log(userAge);
+
+let userCountry = prompt('Where are you from ?')
+console.log(userCountry);
+
+
+let useProfile = alert(`Hello ${userName} your age ${userAge} your country ${userCountry}`);
