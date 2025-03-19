4c795270cb44840aaf0053dd7c829165b7d2777f
Danyil-Sikolenko <banielsikolenko@gmail.com>
2025-03-19 18:12:47 +0100

    Кнопка зміни кольору

---
+++ main.js
@@ -0,0 +1,6 @@
+const mainText = document.querySelector('h1');
+const button = document.querySelector('#button');
+
+button.addEventListener ('click', () => {
+mainText.classList.toggle('chosecolor')
+});
